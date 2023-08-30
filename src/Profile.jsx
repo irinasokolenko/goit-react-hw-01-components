99f54fbe415ea6cbce737b0fe4bf435c681a8ce7
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-08-30 16:05:30 +0300

    hw01

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,9 +1,39 @@
 import user from 'path/to/user.json;
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return(
+    <ProfileWrap>
+      <Description>
+        <Avatar src={avatar} alt = {username} />
+        <Name>{username}</Name>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
 
-<Profile
-  username={user.username}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-/>
+      <Stats>
+        <ListItem>
+          <Label>Followers</Label>
+          <Quanyity>{stats.followers}</Quanyity>
+        </ListItem>
+        <ListItem>
+          <label>Likes</label>
+          <Quanyity>{stats.likes}</Quanyity>
+        </ListItem>
+      </Stats>
+      </ProfileWrap>
+  );
+  };
+  Profile.propTypes = {
+    username: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        followers: PropTypes.number.isRequired,
+        views: PropTypes.number.isRequired,
+        likes: PropTypes.number.isRequired,
+      }).isRequired
+    ).isRequired,
+  };
+  
+  export default Profile;
