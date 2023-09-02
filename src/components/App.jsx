102eca69d94240673f66a77e5dca49f8013944b5
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-09-02 19:42:41 +0300

    hw01

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from '../assets/user.json';
-import data from '../assets/data.json';
-import friends from '../assets/friends.json';
-import transactions from '../assets/transactions.json';
+import user from './assets/user.json';
+import data from './assets/data.json';
+import friends from './assets/friends.json';
+import transactions from './assets/transactions.json';
 
 import { Profile } from "./Profile/Profile";
 import { Statistics } from './Statistics/Statistics';
