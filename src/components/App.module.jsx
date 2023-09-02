117462bf024f81b91e34def363be28e5622b403e
irinasokolenko <127216070+irinasokolenko@users.noreply.github.com>
2023-09-02 19:07:50 +0300

    hw01

---
+++ src/components/App.module.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  text-align: center;
+  width: 412px;
+  margin: auto;
+`
+export const Wrapper = styled.div`
+
+  @media screen and (min-width: 425px){
+    display: flex;
+    justify-content: space-between
+  };
